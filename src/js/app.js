71da0cc7340a3764e405605549228575b3e6792a
Рыбинский Игорь <rybinsky.igor@mail.ru>
2023-12-10 00:49:51 +0800

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -154,3 +154,38 @@ flsFunctions.findVideos();
 
 
 
+
+// content-switcher
+
+
+const tabItems = Array.from(document.querySelectorAll('.tab-item'));
+const contentItems = Array.from(document.querySelectorAll('.content-item'));
+//  Array.from что бы работал .find
+
+const clearActiveClass = (element, className = 'is-active') => {
+  element.find(item => item.classList.remove(`${className}`))
+}
+const setActiveClass = (element, index, className = 'is-active') => {
+  element[index].classList.add(`${className}`)
+}
+
+const checkoutTabs = (item, index) => {
+  item.addEventListener('click', () => {
+
+    if (item.classList.contains('is-active')) return
+
+    clearActiveClass(tabItems)
+    clearActiveClass(contentItems)
+
+    setActiveClass(tabItems, index)
+    setActiveClass(contentItems, index)
+
+  })
+}
+
+tabItems.forEach(checkoutTabs);
+
+
+
+
+
