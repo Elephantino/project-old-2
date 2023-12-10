dc5fc5db859742f537e420e429bee32017539974
Рыбинский Игорь <rybinsky.igor@mail.ru>
2023-12-10 14:14:59 +0800

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -189,3 +189,68 @@ tabItems.forEach(checkoutTabs);
 
 
 
+
+
+
+
+
+
+
+const layoutTabItems = Array.from(document.querySelectorAll('.layout__tab-item'));
+const layoutConTentItems = Array.from(document.querySelectorAll('.layout__content-item'));
+
+const layoutClearActiveClass = (element, className = 'is-active') => {
+  element.find(item => item.classList.remove(`${className}`))
+}
+const layoutSetActiveClass = (element, index, className = 'is-active') => {
+  element[index].classList.add(`${className}`)
+}
+
+const layoutCheckoutTabs = (item, index) => {
+  item.addEventListener('click', () => {
+
+    if (item.classList.contains('is-active')) return
+
+    layoutClearActiveClass(layoutTabItems)
+    layoutClearActiveClass(layoutConTentItems)
+
+    layoutSetActiveClass(layoutTabItems, index)
+    layoutSetActiveClass(layoutConTentItems, index)
+
+  })
+}
+
+layoutTabItems.forEach(layoutCheckoutTabs);
+
+
+
+
+
+
+let svgBoxOne = document.querySelector(".svgBoxOne");
+
+svgBoxOne.addEventListener('click', function () {
+  svgBoxOne.classList.toggle('svgAnimOne');
+});
+
+
+let svgBoxTwo = document.querySelector(".svgBoxTwo");
+
+svgBoxTwo.addEventListener('click', function () {
+  svgBoxTwo.classList.toggle('svgAnimTwo');
+});
+
+
+let svgBoxThree = document.querySelector(".svgBoxThree");
+
+svgBoxThree.addEventListener('click', function () {
+  svgBoxThree.classList.toggle('svgAnimThree');
+});
+
+
+
+let svgBoxFour = document.querySelector(".svgBoxFour");
+
+svgBoxFour.addEventListener('click', function () {
+  svgBoxFour.classList.toggle('svgAnimFour');
+});
